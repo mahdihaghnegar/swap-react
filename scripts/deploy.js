5baa37a2e9692eec78a39ae1c7841d0d017c1142
mahdihaghnegar <mahdi.haghnegar@gmail.com>
2024-03-12 23:42:38 +0330

    deploy to network :fail timeout

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,17 @@
 const hre = require("hardhat");
 
 async function main() {
+  var z = BigInt(1000000000000000000);
+  const a = BigInt(10000) * z; //* 1000000000000000000; // (10 ^ 18);
+  const b = BigInt(20000) * z; //* 1000000000000000000; // (10 ^ 18);
+
   const [deployer] = await ethers.getSigners();
   console.log("Deploying contracts with the account:", deployer.address);
-  //local
+  //local: npx hardhat run scripts/deploy.js
+  //network(with vpn):  npx hardhat run scripts/deploy.js --network sepolia
+  //network start step1:
+  //
+  /*
   const tokenA = await ethers.deployContract("MyTokenA", [deployer]);
   console.log("TokenA address:", await tokenA.getAddress());
   const tokenB = await ethers.deployContract("MyTokenB", [deployer]);
@@ -12,11 +20,11 @@ async function main() {
   const swap = await ethers.deployContract("Swap", [tokenA, tokenB]);
   const swapAddress = await swap.getAddress();
   console.log("swap address:", swapAddress);
-
-  const a = 3000; //* 1000000000000000000; // (10 ^ 18); // 1000000000000000000;
+  //network end step1: (save addreses) and comment all to network setp2
+  //local continiue
+  
   await tokenA.mint(deployer, a); //ok
   console.log("TokenA balanceOf deployer:", await tokenA.balanceOf(deployer));
-  const b = 5000; //* 1000000000000000000; // (10 ^ 18);
   await tokenB.mint(deployer, b); //ok
   console.log("TokenB balanceOf deployer:", await tokenB.balanceOf(deployer));
   await tokenA.approve(swapAddress, a);
@@ -34,45 +42,57 @@ async function main() {
   console.log("Swap get K", await swap.getK());
   //use: npx hardhat run scripts/deploy.js
 
-  /*
+  //end local
+
+  //address from network step 1 
+  /* 
 //new
-TokenA address: 0x5A06c30ea105b69dA3bE428C1e8b491F02F39d8C
-TokenB address: 0xC172ab3A39cae30910888911554f01202bf5B7D3
-swap address: 0x5D65bB645c9d8e812bAE1Eb7baCAb03FD4De99Dc
+swap address: 0x1A16c39286bB3C9D551D7fC9A689d1ace43d4c41
 //old
 TokenA address: 0xfcE79d85c9f7822eaBd2B69A91Eb474640271241
 TokenB address: 0x77Aa7CA18fdC46Ab31D3094b094cdf192F650F39
 swap address: 0x1Cd46004642868d4BDd3610Ca3Bf16DF9F0d6CE6
-  
+  */
+  //network start: step2 coment off if start and comment on if done but next steps not works
   const tokenAaddress = "0xfcE79d85c9f7822eaBd2B69A91Eb474640271241";
   const tokenAcontract = await hre.ethers.getContractFactory("MyTokenA");
   const tokenA = tokenAcontract.attach(tokenAaddress);
-  await tokenA.mint(deployer, 100000 * (10 ^ 18)); //ok
+  //await tokenA.mint(deployer, a); //comment on if it is ok
   console.log("TokenA balanceOf deployer:", await tokenA.balanceOf(deployer));
 
   const tokenBaddress = "0x77Aa7CA18fdC46Ab31D3094b094cdf192F650F39";
   const tokenBcontract = await hre.ethers.getContractFactory("MyTokenB");
   const tokenB = tokenBcontract.attach(tokenBaddress);
-  await tokenB.mint(deployer, 200000 * (10 ^ 18)); //ok
+  //await tokenB.mint(deployer, b); //comment on if it is ok
   console.log("TokenB balanceOf deployer:", await tokenB.balanceOf(deployer));
 
-  /*const swapAddress = "0x1Cd46004642868d4BDd3610Ca3Bf16DF9F0d6CE6";
+  //new swap
+  /* const swap = await ethers.deployContract("Swap", [tokenA, tokenB]);
+  const swapAddress = await swap.getAddress();
+  console.log("swap address:", swapAddress);*/
+
+  //last old swap
+  const swapAddress = "0x1Cd46004642868d4BDd3610Ca3Bf16DF9F0d6CE6"; // "0x1A16c39286bB3C9D551D7fC9A689d1ace43d4c41";
+
   const Swapcontract = await hre.ethers.getContractFactory("Swap");
   const swap = Swapcontract.attach(swapAddress);
 
-  await tokenA.approve(swapAddress, 100000 * (10 ^ 18));
+  //await tokenA.approve(swapAddress, a); //comment on if it is ok
   console.log(
     "Swap allowance Token A:",
     await tokenA.allowance(deployer, swapAddress)
   );
-  await tokenB.approve(swapAddress, 200000 * (10 ^ 18));
+  // await tokenB.approve(swapAddress, b); //comment on if it is ok
   console.log(
     "Swap allowance Token B:",
     await tokenB.allowance(deployer, swapAddress)
   );
 
-  await swap.startCreatePool(100000 * (10 ^ 18), 200000 * (10 ^ 18));
-  console.log("Swap get K", await swap.getK());*/
+  //await swap.startCreatePool(a, b); //comment on if it is ok
+  console.log("Swap get K", await swap.getK());
+  console.log("token A of Swap", await tokenA.balanceOf(swapAddress));
+  console.log("token B of Swap", await tokenB.balanceOf(swapAddress));
+  //end network step2
 }
 
 // We recommend this pattern to be able to use async/await everywhere
